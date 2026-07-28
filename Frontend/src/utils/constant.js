1bf18d06d81f3033c911954cfb928497b8ba7dd3
krish <krishkumar9810@gmail.com>
2026-07-28 08:55:37 +0530

    Frontend : Add prod url

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,6 +1,6 @@
-let PROD_URL = false
+let PROD_URL = true
 
-let BASE_URL = PROD_URL == true ? "" : "http://localhost:3000/api/v1"
+let BASE_URL = PROD_URL == true ? `https://paytm-v1-tchq.onrender.com/api/v1` : "http://localhost:3000/api/v1"
 
 
 export default BASE_URL
